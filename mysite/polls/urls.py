f5eed01f06d6fd6c3a18e45c88f23ff6891bda35
wangpengfei <wangpengfei.pfwang@bytedance.com>
2020-08-19 18:28:34 +0800

    version@0.0.1: generic view concept introduced

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -8,9 +8,9 @@ from . import views
 app_name = 'polls'
 urlpatterns = [
     url(r'^$', views.default_page, name='default_page'),        
-    url(r'^index/', views.index, name='index'),
-    url(r'^(?P<question_id>[0-9]+)/detail/$', views.detail, name='detail'),
-    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    url(r'^index/', views.IndexView.as_view(), name='index'),
+    url(r'^(?P<pk>[0-9]+)/detail/$', views.DetailView.as_view(), name='detail'),
+    url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
     url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
 
