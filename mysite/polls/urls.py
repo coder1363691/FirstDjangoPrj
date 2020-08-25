4002edfe306e8291192e52f8ae239028483cadad
wangpengfei <wangpengfei.pfwang@bytedance.com>
2020-08-25 19:04:12 +0800

    json parse and file-link

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -12,5 +12,10 @@ urlpatterns = [
     url(r'^(?P<pk>[0-9]+)/detail/$', views.DetailView.as_view(), name='detail'),
     url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
     url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+    url(r'^JsonList/', views.JsonListView.as_view(), name='JsonList'),
+    
+    url(r'^root/$', views.root_list, name='root'),
+    url(r'^(?P<node_name>\S+)/children/$', views.node_list, name="node"),
+
 ]
 
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -11,7 +11,41 @@ from django.views import generic
 
 from .models import Question, Choice
 
-# Create your views here.
+import os
+
+import json
+
+global list
+
+list = { 
+        'A':{'file_list':[{'name':'A1', 'contri':0.1234567}, {'name':'A2', 'contri':0.987654}]}, 
+        'A1':{'file_list':[{'name':'B1', 'contri':0.1234567}, {'name':'B2', 'contri':0.987654}]}, 
+        'A2':{'file_list':[{'name':'C1', 'contri':0.1234567}, {'name':'C2', 'contri':0.987654}]}, 
+        }
+
+
+
+global meta_dict
+
+json_file='./meta.json'
+meta_dict = {}
+
+with open(json_file) as fd1:
+    message = json.load(fd1)
+
+
+## fot github test
+for item in message:
+    meta_dict[item] = message[item]
+
+print meta_dict
+
+for item in meta_dict:
+    print item
+    print ':::::::::::::'
+    for file_info in meta_dict[item]['file_list']:
+        print file_info
+    print '\n'
 
 def default_page(request):
     return HttpResponse("Welcome, default page of APP polls")
@@ -41,6 +75,35 @@ def vote(request, question_id):
         selected_choice.votes += 1
         selected_choice.save()
         return HttpResponseRedirect(reverse('polls:results', args=(question.id, )))
+
+class JsonListView(generic.ListView):
+    context_object_name = 'children_list'
+    template_name = 'polls/jsonlist.html'
+
+    def get_queryset(self):
+        json_file = "/home/wangpengfei.pfwang/devroot/000/workdir/django/ByGithub/FirstDjangoPrj/mysite/meta.json"
+        with open(json_file) as fd:
+                message = json.load(fd)
+
+        children_list = []
+
+        for obj in message['object']:
+            if message['object'][obj]['children']:
+                for inc in message['object'][obj]['children']:
+                    one = {'contri': inc['contribution'], 'name': inc['name']}
+                    children_list.append(one)
+
+        #children_list = ['a_file', 'b_file', 'c_file']
+        return children_list
+
+def root_list(request):
+    file_list = meta_dict['root']
+    return render(request, 'polls/list.html', file_list)
+
+def node_list(request, node_name):
+    file_list = meta_dict[node_name]
+    return render(request, 'polls/list.html', file_list)
+
 
 
 
